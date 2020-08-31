ceebb3a248468b5893f693e8466daea21f2f938c
Romart <rmf1312@hotmail.com>
2020-08-31 14:47:13 +0000

    Created check out app and models

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
 
+from products.models import Product
 
 def view_cart(request):
     """ A view that renders the cart contents page """
@@ -10,6 +12,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -19,15 +22,18 @@ def add_to_cart(request, item_id):
     else:
         cart[item_id] = quantity
 
+    messages.success(request, f'Added {product.name} to your cart')
+
     request.session['cart'] = cart
-    print(request.session['cart'])
     return redirect(redirect_url)
 
 
 def update_cart(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
+
     cart = request.session.get('cart', {})
 
     request.session['cart'] = cart
