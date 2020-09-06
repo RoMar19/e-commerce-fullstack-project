08dfb98cb55e4c81a92b6de6da7e56b484490fba
Romart <rmf1312@hotmail.com>
2020-09-06 19:57:32 +0000

    Created heroku app, added Postgres and superuser

--- profiles/views.py
+++ profiles/views.py
@@ -1,36 +1,41 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 
 from checkout.models import Order
 
-if request.method == 'POST':
+
+@login_required
+def profile(request):
+    """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request,
+                           ('Update failed. Please ensure '
+                            'the form is valid.'))
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
-from .models import UserProfile
-
-
-def profile(request):
-    """ Display the user's profile. """
-    profile = get_object_or_404(UserProfile, user=request.user)
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'orders': orders,
-        'on_profile_page': True 
+        'on_profile_page': True
     }
 
     return render(request, template, context)
 
-    def order_history(request, order_number):
+
+def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
