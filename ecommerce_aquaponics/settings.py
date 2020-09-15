a6ff90fcdeeb2f4059b4edcf30fd2dfaca7a25a1
Romart <rmf1312@hotmail.com>
2020-09-15 15:54:27 +0000

    DB changed

--- ecommerce_aquaponics/settings.py
+++ ecommerce_aquaponics/settings.py
@@ -125,6 +125,8 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+print(DATABASES)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
