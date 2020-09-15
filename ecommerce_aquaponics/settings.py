59191aa142624edd8ea82f1549500c4c50c26902
Romart <rmf1312@hotmail.com>
2020-09-15 09:09:52 +0000

    New superuser created

--- ecommerce_aquaponics/settings.py
+++ ecommerce_aquaponics/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['aquaponics-shop.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['localhost', 'aquaponics-shop.herokuapp.com']
 
 
 # Application definition
