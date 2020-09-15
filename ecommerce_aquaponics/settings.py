afe827988793f9671f2eb982a5dab56e4b461b0d
Romart <rmf1312@hotmail.com>
2020-09-15 08:07:18 +0000

    APP_DIRS back to True

--- ecommerce_aquaponics/settings.py
+++ ecommerce_aquaponics/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
             os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
-        'APP_DIRS': False,
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
